72114466ed6c47f7abc36359e734ac2e415a1f28
Mike Martley <martleym@gmail.com>
2020-03-13 11:27:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,28 +2,26 @@ from flask import Flask
 from google.cloud import automl_v1beta1 as automl
 app = Flask(__name__)
 
+# AutoML requirements
 project_id = 'production-268923'
 compute_region = 'us-central1'
 model_display_name = 'model_prep_20200310053214'
-inputs = {'trip_seconds': 600,'trip_miles': 4}
+inputs = {"trip_seconds": 614, "trip_miles": 3.2}
 
-client = automl.TablesClient(project=project_id, region=compute_region)
-
+# homepage
 @app.route('/')
-def results():
-    response = client.predict(
-        model_display_name=model_display_name, inputs=inputs
-    )
-    print("Prediction results:")
-    for result in response.payload:
-        print(
-            "Prediction: {}".format(result.tables.value)
-        )
+def placeholder():
+    return "Taxi Fare Predictions"
 
-    
+# return predictions
+@app.route('/predict', methods=['GET', 'POST'])
+def get_result():
+    client = automl.TablesClient(project=project_id, region=compute_region)
+    response = client.predict(model_display_name=model_display_name, inputs=inputs)
+    return str(response.payload)
 
 if __name__ == '__main__': 
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run()
 
 
 
